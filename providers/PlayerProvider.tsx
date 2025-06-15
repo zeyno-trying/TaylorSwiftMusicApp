fcb043776283830ec3253c72d62d90c5dbf8b821
Zineb153 <zinebdjebbari610@gmail.com>
2025-06-15 14:06:36 +0100

    define playerContext

--- providers/PlayerProvider.tsx
+++ providers/PlayerProvider.tsx
@@ -1,1 +1,24 @@
-import { useContext, createContext } from "node_modules/@types/react";
+import { PropsWithChildren, createContext, useContext, useState } from 'react';
+import { Track } from '../types';
+
+type PlayerContextType = {
+  track?: Track;
+  setTrack: (track: Track) => void;
+};
+
+const PlayerContext = createContext<PlayerContextType>({
+  track: undefined,
+  setTrack: () => {},
+});
+
+export default function PlayerProvider({ children }: PropsWithChildren) {
+  const [track, setTrack] = useState<Track>();
+
+  return (
+    <PlayerContext.Provider value={{ track, setTrack }}>
+      {children}
+    </PlayerContext.Provider>
+  );
+}
+
+export const usePlayerContext = () => useContext(PlayerContext);
